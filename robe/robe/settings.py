b4bfd8ebaf5027222f4adbac288b7ebfe2c827a0
Antriksh <thechef@mymail.antriksh.com>
2015-06-11 09:00:27 +0530

    starting with django app

--- robe/robe/settings.py
+++ robe/robe/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'robe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'flamingtribble',
+        'USER': 'root',
+        'PASSWORD': 'tribble',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
