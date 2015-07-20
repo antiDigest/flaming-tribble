8e2c4efc936a43cef80f7d0be69d5e9df9b00040
Antriksh <antriksh5235@gmail.com>
2015-07-20 13:12:22 +0530

    modified settings

--- robe/robe/settings.py
+++ robe/robe/settings.py
@@ -109,4 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+     STATIC_PATH,
+      )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
