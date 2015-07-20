1d9b4fc122e1a1c3c0e2b3259b01f96a256a41b9
Antriksh <antriksh5235@gmail.com>
2015-07-20 13:44:39 +0530

    register

--- robe/tribble/admin.py
+++ robe/tribble/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Words_India, Words_Global, ytstats, tstats, fbstats
 
 # Register your models here.
+
+admin.site.register(Words_India)
+admin.site.register(Words_Global)
+admin.site.register(ytstats)
+admin.site.register(fbstats)
+admin.site.register(tstats)
